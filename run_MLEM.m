7e0b1db07762e99a976d863999d75c402f8ce70d
Sarah Van Hoesen <svanhoesen@wpi.edu>
2026-05-22 11:47:47 -0400

    projection input
    now works with input projections from simind (or other).

--- run_MLEM.m
+++ run_MLEM.m
@@ -1,8 +1,8 @@
 %% Basic Input Parameters:
-phantom_type = 'simple';
-phantom_size = 32;
-iteration_num = 3; % Number of MLEM iterations to process
-sample_points = 30; % Number of angles to use in (back)projection
+phantom_type = 'simindproj';
+phantom_size = 128;
+iteration_num = 10; % Number of MLEM iterations to process
+sample_points = 120; % Number of angles to use in (back)projection
 collimator_type = 'parallel';
 
 % Initialize collimator parameters
@@ -17,7 +17,6 @@ septal_angle = 90; % PINHOLE: Angle in degrees of the pinhole
 detector_elements = 32; % readjust when uncoupled from the body size
 pixel_size = 0.22; % Absolute length of detector pixel in cm
 body_size = [phantom_size phantom_size phantom_size];
-body_length = phantom_size;
 % length_total = phantom_size^3;
 % Create an array with the angles that are being imaged
 steps = 360/(sample_points);
@@ -26,7 +25,7 @@ theta(end) = []; % this keeps to the number of sample points (no repeat at 360=0
 clear('steps')
 
 %% Create the collimator
-dist_array = 1:body_length;
+dist_array = 1:phantom_size;
 dist_array = dist_array*pixel_size; % currently assumes there is no space between the pixels (NOT physical)
 
 if strcmp(collimator_type,'pinhole')
@@ -61,6 +60,13 @@ elseif strcmp(phantom_type,'kidneyxcat')
 
 elseif strcmp(phantom_type,'simindproj')
     % Read in projections from simind and process them without the original image available
+    location = 'C:\Users\svanhoesen\OneDrive - Worcester Polytechnic Institute (wpi.edu)\matlab_testing\projections\';
+    left = Read_XCAT(append(location,'lkd128_ppsc.prj'),128);
+    right = Read_XCAT(append(location,'rkd128_ppsc.prj'),128);
+    lesion = Read_XCAT(append(location,'ls1128_ppsc.prj'),128);
+    projections = right + left - lesion;
+    
+    clear('right','left','lesion')
 
 elseif strcmp(phantom_type,'simple')
     image_space = ones(body_size);
@@ -68,7 +74,7 @@ elseif strcmp(phantom_type,'simple')
     center = [10 10 10];
     radius = 2;
     og_image = insert_3d_phantom(sag,cor,tra,center,radius);
-    og_image = reshape(og_image,body_length,body_length,body_length);
+    og_image = reshape(og_image,phantom_size,phantom_size,phantom_size);
     og_image = rescale(og_image,0,255);
     clear('sag','cor','tra','image_space')
 
@@ -77,11 +83,33 @@ elseif strcmp(phantom_type,'simple')
 end
 
 %% Call the MLEM_iteration script
-recon_final = mlem_iteration(projections,theta,body_size,collimator,iteration_num);
+recon_final = mlem_iteration(projections,theta,body_size,collimator,iteration_num,'plot_robust',true);
 
 
 
 
 
-
+% % % Attempt the FFT method mentioned in Sorenson pg 398 (only for known og image)
+% % fft3d_og = fftn(og_image);
+% % fft3d_recon = fftn(recon_final);
+% % fft3d_ratio = fft3d_og./fft3d_recon;
+% % 
+% % correction = ifftn(fft3d_ratio);
+% % image_test = convn(fft3d_recon,correction,'same');
+% % 
+% % figure()
+% % montage(uint16(og_image),DisplayRange=[])
+% % title('OG Image')
+% % 
+% % figure()
+% % montage(uint16(recon_final),DisplayRange=[])
+% % title('Recon Image')
+% % 
+% % figure()
+% % montage(uint16(image_test),DisplayRange=[])
+% % title('Convolution Filtered Image')
+% % 
+% % figure()
+% % montage(uint16(correction),DisplayRange=[])
+% % title('Convolution Filter')
 
